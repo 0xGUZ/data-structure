7956e9628dc358134486831a0ab5546e930dd0f2
gustavo-villar-dev <gustavovm@dcc.ufrj.br>
2023-04-05 23:44:10 -0300

    [nf] template

---
+++ template.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include "headers/ds.h"
+
+clock_t start, end;
+
+/*TEMPLATE SORT
+time complexity - worst, avg, best = O( )
+space complexity - worst, avg, best = O( )*/
+
+void renameThis(int arr[], int n){
+    int i, j;
+
+}
+
+int main(){
+    int size = 1000;
+    int* arr = generateRandomArray(size);
+
+    start = clock();
+
+        renameThis(arr, size);
+
+    end = clock();
+
+        printTime(start,end);
+        checkIfSorted(arr,size);
+
+    free(arr);
+    return 0;
+}
+
+/*  gcc renameThis-sort.c headers/ds.c -ansi -pedantic -Wall -o renameThis-sort && ./renameThis-sort  */
